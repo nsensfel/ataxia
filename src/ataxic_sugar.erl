e2fbb6b493cc8a244bb70bd0522a327030b1d7ec
Nathanael Sensfelder <SpamShield0@MultiAgentSystems.org>
2018-12-21 17:24:30 +0100

    ...

--- src/ataxic_sugar.erl
+++ src/ataxic_sugar.erl
@@ -10,7 +10,9 @@
 -export
 (
    [
-      update_array_cell/2
+      update_array_cell/2,
+      update_orddict_element/2,
+      nop/0
    ]
 ).
 
@@ -47,3 +49,38 @@ update_array_cell (IX, OP) ->
          ataxic:current_value()
       ]
    ).
+
+-spec update_orddict_element
+   (
+      non_neg_integer(),
+      ataxic:basic()
+   )
+   -> ataxic:basic().
+update_orddict_element (IX, OP) ->
+   ataxic:apply_function
+   (
+      orddict,
+      store,
+      [
+         ataxic:constant(IX),
+         ataxic:sequence
+         (
+            [
+               ataxic:apply_function
+               (
+                  orddict,
+                  fetch,
+                  [
+                     ataxic:constant(IX),
+                     ataxic:current_value()
+                  ]
+               ),
+               OP
+            ]
+         ),
+         ataxic:current_value()
+      ]
+   ).
+
+-spec nop () -> ataxic:basic().
+nop () -> ataxic:sequence([]).
