a3c1bfdf5d95184a2d5ffad0523872cb2fadbb33
Nathanaël Sensfelder <spamshield+github.com@segfault.tech>
2025-09-05 11:32:42 +0200

    Better typing for client data.

--- src/ataxia_client_data.erl
+++ src/ataxia_client_data.erl
@@ -19,9 +19,10 @@
 	}
 ).
 
--type type() :: #data{}.
+-type type(DataType) :: #data{value :: DataType}.
+-type type() :: type(any()).
 
--export_type([type/0]).
+-export_type([type/0, type/1]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% EXPORTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -56,9 +57,9 @@
 		ataxia_id:type(),
 		ataxia_network:proc(),
 		non_neg_integer(),
-		any()
+		A
 	)
-	-> type().
+	-> type(A).
 new (DB, ID, Lock, Version, Value) ->
 	#data
 	{
@@ -70,7 +71,7 @@ new (DB, ID, Lock, Version, Value) ->
 		lock = Lock
 	}.
 
--spec add_update (ataxic:type(), any(), type()) -> type().
+-spec add_update (ataxic:type(), A, type(A)) -> type(A).
 add_update (Update, NewValue, ClientData) ->
 	ClientData#data
 	{
@@ -82,10 +83,10 @@ add_update (Update, NewValue, ClientData) ->
 	(
 		ataxia_network:proc(),
 		non_neg_integer(),
-		any(),
-		type()
+		A,
+		type(A)
 	)
-	-> type().
+	-> type(A).
 update_to (NewLock, NewVersion, NewValue, ClientData) ->
 	ClientData#data
 	{
@@ -94,21 +95,21 @@ update_to (NewLock, NewVersion, NewValue, ClientData) ->
 		value = NewValue
 	}.
 
--spec set_lock (ataxia_network:proc(), type()) -> type().
+-spec set_lock (ataxia_network:proc(), type(A)) -> type(A).
 set_lock (NewLock, ClientData) -> ClientData#data{ lock = NewLock }.
 
--spec get_database (type()) -> atom().
+-spec get_database (type(_)) -> atom().
 get_database (#data{ db = Result }) -> Result.
 
--spec get_id (type()) -> ataxia_id:type().
+-spec get_id (type(_)) -> ataxia_id:type().
 get_id (#data{ id = Result }) -> Result.
 
--spec get_lock (type()) -> ataxia_network:proc().
+-spec get_lock (type(_)) -> ataxia_network:proc().
 get_lock (#data{ lock = Result }) -> Result.
 
--spec get_value (type()) -> any().
+-spec get_value (type(A)) -> A.
 get_value (#data{ value = Result }) -> Result.
 
--spec get_ataxic (type()) -> ataxic:type().
+-spec get_ataxic (type(_)) -> ataxic:type().
 get_ataxic (#data{ updates = Updates }) ->
 	ataxic:sequence(lists:reverse(Updates)).
